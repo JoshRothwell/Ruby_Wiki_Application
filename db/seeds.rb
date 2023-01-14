63770ced6e0655576dfe0cd197c2974203519a79
JoshRothwell <x19237081@student.ncirl.ie>
2023-01-14 05:33:11 +0000

    Final commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(email: 'josh@example.com', password: 'password', password_confirmation: 'password')
 
 10.times do |x|
-    Post.create(title: "Title #{x}", body: "Body #{x} Words go here, this is a test document.")
+    Post.create(title: "Forum Post #{x}", body: "Body #{x} This an automatically generated forum post, feel free to delete this post or edit it to your liking.", user_id: User.first.id)
 end
